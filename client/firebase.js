4d85c714d69d57060ed024c5b15f2f48bf64545d
codeytynan29 <codeytynan29@gmail.com>
2025-06-05 11:47:35 -0400

    Update firebase.js

--- client/firebase.js
+++ client/firebase.js
@@ -1,15 +1,21 @@
-// Replace the values below with your actual Firebase project config
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB0t_5fdgAEyXTaLhMqbRmZm0SGORowqDU",
+  authDomain: "smartflow-c0fb5.firebaseapp.com",
+  projectId: "smartflow-c0fb5",
+  storageBucket: "smartflow-c0fb5.firebasestorage.app",
+  messagingSenderId: "334938120915",
+  appId: "1:334938120915:web:320600cc1e0c5e141caa26",
+  measurementId: "G-8QDE3GEQQ3"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
